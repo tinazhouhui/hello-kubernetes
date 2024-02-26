ddc57d1980cdc776c1b71d43c5929bb79603eff9
tinazhouhui <tina.zhou.hui@gmail.com>
2024-02-26 17:32:15 +0100

    feat: return env variables

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ import express from 'express';
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.json()); // json body parser
 app.get('/', function (req, res) {
     const dirname = new URL('.', import.meta.url).pathname;
     res.sendFile(dirname + '/' + 'index.html');
-})
+});
+
+app.get('/pod', function (req, res) {
+    res.send({hostname: process.env.HOSTNAME, home: process.env.HOME, env: process.env})
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {  // initialize server
